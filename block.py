fa4daf2e56af51c919a5a13e17d8f8cf19aa2036
Devansh Srivastav <devansh.srivastav@gmail.com>
2025-03-17 10:49:16 +0100

    Add files via upload

---
+++ block.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+from attention import MultiHeadAttention
+from mlp import MLP
+
+class GPTBlock(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = MultiHeadAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
